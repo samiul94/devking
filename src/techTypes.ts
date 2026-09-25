e37c70f01cfc7d5730b081e691f50bd1e505a3fa
samiul94 <saaamiul1994@gmail.com>
2026-09-25 22:50:19 +0600

    First commit: intialize react ts project

---
+++ src/techTypes.ts
@@ -0,0 +1,14 @@
+export type Technology = {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  level: string;
+  rating: number;
+  badge: string;
+  badgeClass: string;
+  icon: string;
+};
+
+
+
